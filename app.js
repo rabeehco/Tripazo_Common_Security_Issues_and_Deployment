61310e1ac8962570bcb38f81410a84a96a4cb8c9
codewithrabeeh <real.ray48@gmail.com>
2022-07-18 17:48:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+
+
+const express = require('express')
+const app = express()
+const path = require('path')
+const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate')
+const session = require('express-session')
+const ExpressError = require('./utils/ExpressError')
+const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
+
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:/yelp-camp')
+.then(()=>{
+    console.log('DB connected')
+})
+
+
+const Campground = require('./models/campground')
+
+
+/* Setting View Engine for EJS */
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views')) 
+/* For Parsing The Json Data in Browser */
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly:true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
+
+
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+/* Render Home.ejs */
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found'))
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', {err})
+    // res.send('Oh Boy, Something Went Wrong!')
+})
+
+app.listen(3000, () => {
+    console.log('Serving on Port 3000')
+})
