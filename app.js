4f4c4eb414975df67d557e75c6308fa7f2271887
codewithrabeeh <real.ray48@gmail.com>
2022-07-18 20:36:57 +0530

    helmet

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
+// if(process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config();
+// }
+require('dotenv').config();
 
 
 const express = require('express')
@@ -15,6 +15,7 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const helmet = require('helmet')
 
 const mongoSanitize = require('express-mongo-sanitize')
 
@@ -57,6 +58,53 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 app.use(flash())
+// app.use(helmet({ contentSecurityPolicy: false }))
+
+const scriptSrcUrls = [
+ 
+    'https://stackpath.bootstrapcdn.com/',
+    'https://api.tiles.mapbox.com/',
+    'https://api.mapbox.com/',
+    'https://kit.fontawesome.com/',
+    'https://cdnjs.cloudflare.com/',
+    'https://cdn.jsdelivr.net/'
+  ];
+  const styleSrcUrls = [
+    'https://kit-free.fontawesome.com/',
+    'https://stackpath.bootstrapcdn.com/',
+    'https://api.mapbox.com/',
+    'https://api.tiles.mapbox.com/',
+    'https://fonts.googleapis.com/',
+    'https://use.fontawesome.com/',
+    'https://cdn.jsdelivr.net/'  // I had to add this item to the array 
+  ];
+  const connectSrcUrls = [
+    'https://api.mapbox.com/',
+    'https://a.tiles.mapbox.com/',
+    'https://b.tiles.mapbox.com/',
+    'https://events.mapbox.com/'
+  ];
+  const fontSrcUrls = [];
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", 'blob:'],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          'blob:',
+          'data:',
+          'https://res.cloudinary.com/deuyeqft4/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+          'https://images.unsplash.com/'
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls]
+      }
+    })
+  );
 
 app.use(passport.initialize())
 app.use(passport.session())
