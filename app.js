3a91c01c833b0dcacf189e49ca5a38cd0304fd1b
codewithrabeeh <real.ray48@gmail.com>
2022-07-18 20:01:33 +0530

    minor changes to session cookies

--- app.js
+++ app.js
@@ -43,11 +43,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize())
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly:true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
