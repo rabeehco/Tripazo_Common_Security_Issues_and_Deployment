c7f0791ac9024e864b479c55d892d8555fdd6642
codewithrabeeh <real.ray48@gmail.com>
2022-07-18 17:54:48 +0530

    mongo injection

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 
+const mongoSanitize = require('express-mongo-sanitize')
+
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
@@ -38,6 +40,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
@@ -61,6 +64,7 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
+    console.log(req.query)
     res.locals.currentUser = req.user
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
